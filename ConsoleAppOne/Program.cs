4612dc523a66056cb5c8edd4f8d44d7e03dbe7d8
Josip Harci <harcijosip99@gmail.com>
2023-03-07 17:30:29 +0100

    Update

---
+++ ConsoleAppOne/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+// 
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your name");
+
+        String UserName = Console.ReadLine();
+
+        Console.WriteLine("Hello " + UserName);
+
+        Console.ReadLine();
+    }
+}
