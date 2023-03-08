63e3d9f4bc6d254c3362193d1587ed3010088a98
Josip Harci <harcijosip99@gmail.com>
2023-03-08 15:29:48 +0100

    Update Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -4,10 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("   /|");
-            Console.WriteLine("  / |");
-            Console.WriteLine(" /  |");
-            Console.WriteLine("/___|");
+
+            // A drawn triangle
+
+            /*
+             Console.WriteLine("   /|");
+             Console.WriteLine("  / |");
+             Console.WriteLine(" /  |");
+             Console.WriteLine("/___|");
+            */
+
+            // Variables
+
+            Console.WriteLine("There once was a man named Josip");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name Josip");
+            Console.WriteLine("But didn't like being 70");
+
+
 
             Console.ReadLine();
         }
