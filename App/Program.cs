0196351d6b8913dbea35dfab42fc81ad7b33cf8c
Josip Harci <harcijosip99@gmail.com>
2023-03-11 17:57:40 +0100

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -147,6 +147,19 @@
             Console.WriteLine("I love " + celebrity);
             */
 
+            // Arrays
+
+            /*
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42};
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "John";
+
+            luckyNumbers[1] = 900;
+
+            Console.WriteLine(luckyNumbers[1] );
+            */
+
             Console.ReadLine();
 
         }
