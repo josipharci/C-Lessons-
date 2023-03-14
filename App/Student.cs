c76fb6052f7ac3aaee29e363c4acf230e08b271f
Josip Harci <harcijosip99@gmail.com>
2023-03-14 12:47:46 +0100

    Update

--- App/Student.cs
+++ App/Student.cs
@@ -8,6 +8,15 @@ namespace App
 {
     class Student
     {
+        public string name;
+        public string major;
+        public double gpa;
 
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
     }
 }
