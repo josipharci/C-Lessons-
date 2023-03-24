f6e72df1d5ed80033f5250373a575c2c9ce23140
Josip Harci <harcijosip99@gmail.com>
2023-03-24 11:10:41 +0100

    Update

--- AppForLessons/Form3.Designer.cs
+++ AppForLessons/Form3.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.SuspendLayout();
             // 
             // Form3
@@ -35,8 +36,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form3";
-            this.Text = "+";
+            this.Text = "Intellitest";
             this.ResumeLayout(false);
 
         }
